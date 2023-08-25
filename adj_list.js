1c0074cc116e14df82bed845f9263e9bbe9b9f79
Gabriel Tornich <61199629+wTornich@users.noreply.github.com>
2023-08-25 01:04:19 +0000

    10/08 Classe Graph e métodos de inserção de arestas

--- adj_list.js
+++ adj_list.js
@@ -1,7 +1,6 @@
-
 import Graph from './lib/graph.class.js'
 
-const g = new Graph()
+const g = new Graph(true)
 
 // Adicionando os vértices
 g.addVertex('A')
@@ -9,14 +8,42 @@ g.addVertex('B')
 g.addVertex('C')
 g.addVertex('D')
 g.addVertex('E')
+g.addVertex('F')
 
 // Adicionando as arestas
-
 g.addEdge('A', 'C')
 g.addEdge('B', 'C')
-g.addEdge('D', 'C')
-g.addEdge('D', 'E')
 g.addEdge('B', 'E')
+g.addEdge('C', 'D')
+g.addEdge('D', 'E')
+
+console.log(g)
+
+// Removendo o vértice F
+//g.removeVertix('F')
+//console.log('~> Vértice F removido')
+//console.log(g)
+
+// TENTATIVA de remoção do vértice B
+// g.removeVertix('B')
+
+/*****************************************************/
+
+// Criação de um grafo direcionado
+//const dg = new Graph(true)
+
+//dg.addEdge('V1', 'V2')
+//dg.addEdge('V2', 'V3')
+//dg.addEdge('V2', 'V4')
+//dg.addEdge('V4', 'V1')
+
+//console.log('--------------------------------------------------')
+//console.log(dg)
+
+// TENTATIVA de excluir o vértice V3
+//dg.removeVertix('V3')
+
+g.removeEdge('B', 'E')
 
 console.log(g)
 
